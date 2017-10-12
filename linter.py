2049bc2f97c6db89ca13c102d71e441c1bec1aa5
Alexey-t <support@uvviewsoft.com>
2017-10-12 12:23:08 +0300

    init

---
+++ linter.py
@@ -0,0 +1,24 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Jan T. Sott
+# Copyright (c) 2017 Jan T. Sott
+#
+# License: MIT
+# Ported to CudaLint by Alexey T.
+#
+
+from cuda_lint import Linter, util
+
+class Iscc(Linter):
+    """Provides an interface to the ISCC executable."""
+
+    cmd = ('ISCC.exe', '/Q', '/O-', '@')
+    syntax = 'Inno Setup'
+    regex = (
+        r'^Error on line (?P<line>\d+) in (?P<file>.*\.iss): (?P<message>.+)$'
+    )
+    multiline = True
+    error_stream = util.STREAM_STDERR
+    line_col_base = (1, 1)
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Linter for CudaLint.
+Supports Inno Setup lexer.
+
+You must have Inno Setup installed, and file ISCC.exe must be in system PATH.
+
+Ported from SublimeLinter-contrib-iscc by Alexey T.
